29f8dda6e7ff399effd009d8928cffd45d99c750
sethvincent <sethvincent@gmail.com>
2018-01-18 16:01:07 -0500

    wip sled handler

---
+++ tests/handler.py
@@ -0,0 +1,22 @@
+import unittest
+
+from handler import sled_handler
+
+class TestSledHandler(unittest.TestCase):
+
+    def test_working(self):
+        self.assertTrue(sled_handler)
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+    # 
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
