84af1520c80841669278a03892917ff356bc1b94
sethvincent <sethvincent@gmail.com>
2018-01-19 18:19:58 -0500

    test works

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -31,15 +31,17 @@ def run_test_handler(event, handler_fn):
         }
     }
 
-    handler(event, {}, handler_fn, handler_config) 
+    return handler(event, {}, handler_fn, handler_config) 
 
 class TestSledHandler(unittest.TestCase):
 
-    def test_working(self):
+    def test_simple_handler(self):
         def test_fn(event, context):
             return event
 
         response = run_test_handler(create_event(), test_fn)
+        self.assertTrue(response['cumulus_meta']['task'] == 'Example')
+        self.assertTrue(response['payload']['input']['anykey'] == 'anyvalue')
         self.assertTrue(response)
 
 if __name__ == "__main__":
