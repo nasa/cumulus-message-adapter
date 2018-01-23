671a9e6d11de34c0fa4f75e21fa9c5c820331699
Scisco <scisco7@gmail.com>
2018-01-23 11:59:10 -0500

    allow different local stack hostname

--- message/aws_sled.py
+++ message/aws_sled.py
@@ -1,10 +1,17 @@
-import boto3
-import os
+""" Determines the correct AWS endpoint for AWS services """
 
-localhost_s3_url = 'http://localhost:4572'
+import os
+import boto3
+
 
 def s3():
-  if ('ENV' in os.environ) and (os.environ["ENV"] == 'testing'):
-    return boto3.resource(service_name='s3', endpoint_url=localhost_s3_url)
-  else:
+    """ Determines the endpoint for the S3 service """
+
+    if 'LOCALSTACK_HOST' in os.environ:
+        localhost_s3_url = 'http://%s:4572' % os.environ['LOCALSTACK_HOST']
+    else:
+        localhost_s3_url = 'http://localhost:4572'
+
+    if ('ENV' in os.environ) and (os.environ['ENV'] == 'testing'):
+        return boto3.resource(service_name='s3', endpoint_url=localhost_s3_url)
     return boto3.resource('s3')
