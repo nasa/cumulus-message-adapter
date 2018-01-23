345d85a53393b5baf43fc7d102866c82d7eafc50
Scisco <scisco7@gmail.com>
2018-01-23 10:36:41 -0500

    more linting

--- tests/test.py
+++ tests/test.py
@@ -7,98 +7,46 @@ from mock import patch
 
 from message import aws_sled, message
 
-# Setup objects for testing
-sled_message = message.message()
-s3 = aws_sled.s3()
-bucket_name = 'testing-bucket'
-key_name = 'blue_whale-event.json'
-s3_object = {'input': ':blue_whale:'}
-
-# Event objects
-event_with_replace = {'replace': {'Bucket': bucket_name, 'Key': key_name}}
-event_without_replace = {'input': ':baby_whale:'}
-event_with_ingest = {
-    'ingest_meta': {
-        'message_bucket': bucket_name
-    },
-    'cumulus_meta': {
-        'workflow': 'testing'
-    }
-}
-nested_event_local = {
-    "workflow_config": {
-        "Example": {
-            "bar": "baz",
-            "cumulus_message": {
-                "input": "{{$.payload.input}}",
-                "outputs": [{"source": "{{$.input.anykey}}",
-                             "destination": "{{$.payload.out}}"}]
-            }
-        }
-    },
-    "cumulus_meta": {"task": "Example", "message_source": "local", "id": "id-1234"},
-    "meta": {"foo": "bar"},
-    "payload": {"input": {"anykey": "anyvalue"}}
-}
-nested_event_local_return = {
-    'input': {'anykey': 'anyvalue'},
-    'config': {'bar': 'baz'},
-    'messageConfig': {
-        'input': '{{$.payload.input}}',
-        'outputs': [{'source': '{{$.input.anykey}}',
-                     'destination': '{{$.payload.out}}'}]}
-}
-
-# nestedResponse objects
-nestedResponse = {
-    'input': {
-        'dataLocation': 's3://source.jpg'
-    }
-}
-
-# messageConfig objects
-message_config_with_simple_outputs = {
-    'outputs': [{
-        'source': '{{$.input.dataLocation}}',
-        'destination': '{{$.payload}}'
-    }]
-}
-message_config_with_nested_outputs = {
-    'outputs': [{
-        'source': '{{$.input.dataLocation}}',
-        'destination': '{{$.payload.dataLocation}}'
-    }]
-}
-message_config_without_outputs = {}
-
-test_uuid = 'aad93279-95d4-4ada-8c43-aa5823f8bbbc'
-next_event_object_key_name = "events/{0}".format(test_uuid)
-
-
 class Test(unittest.TestCase):
     """ Test class """
 
+    s3_object = {'input': ':blue_whale:'}
+    bucket_name = 'testing-bucket'
+    key_name = 'blue_whale-event.json'
+    event_with_replace = {'replace': {'Bucket': bucket_name, 'Key': key_name}}
+    event_without_replace = {'input': ':baby_whale:'}
+    test_uuid = 'aad93279-95d4-4ada-8c43-aa5823f8bbbc'
+    next_event_object_key_name = "events/{0}".format(test_uuid)
+    s3 = aws_sled.s3()
+    sled_message = message.message()
+
     def setUp(self):
-        s3.Bucket(bucket_name).create()
-        s3.Object(bucket_name, key_name).put(Body=json.dumps(s3_object))
+        self.nested_response = {
+            'input': {
+                'dataLocation': 's3://source.jpg'
+            }
+        }
+
+        self.s3.Bucket(self.bucket_name).create()
+        self.s3.Object(self.bucket_name, self.key_name).put(Body=json.dumps(self.s3_object))
 
     def tearDown(self):
         delete_objects_object = {
-            'Objects': [{'Key': key_name}, {'Key': next_event_object_key_name}]
+            'Objects': [{'Key': self.key_name}, {'Key': self.next_event_object_key_name}]
         }
-        s3.Bucket(bucket_name).delete_objects(Delete=delete_objects_object)
-        s3.Bucket(bucket_name).delete()
+        self.s3.Bucket(self.bucket_name).delete_objects(Delete=delete_objects_object)
+        self.s3.Bucket(self.bucket_name).delete()
 
     # loadRemoteEvent tests
     def test_returns_remote_s3_object(self):
         """ Test remote s3 event is returned when 'replace' key is present """
-        result = sled_message.loadRemoteEvent(event_with_replace)
-        assert result == s3_object
+        result = self.sled_message.loadRemoteEvent(self.event_with_replace)
+        assert result == self.s3_object
 
     def test_returns_event(self):
         """ Test event argument is returned when 'replace' key is not present """
-        result = sled_message.loadRemoteEvent(event_without_replace)
-        assert result == event_without_replace
+        result = self.sled_message.loadRemoteEvent(self.event_without_replace)
+        assert result == self.event_without_replace
 
     # loadNestedEvent tests
     def test_returns_loadNestedEvent_local(self):
@@ -109,25 +57,60 @@ class Test(unittest.TestCase):
            with 'cumulus_message' excluded
         - 'messageConfig' in return value is the cumulus_message.input of the task configuration
         """
-        result = sled_message.loadNestedEvent(nested_event_local, {})
+
+        nested_event_local = {
+            "workflow_config": {
+                "Example": {
+                    "bar": "baz",
+                    "cumulus_message": {
+                        "input": "{{$.payload.input}}",
+                        "outputs": [{"source": "{{$.input.anykey}}",
+                                    "destination": "{{$.payload.out}}"}]
+                    }
+                }
+            },
+            "cumulus_meta": {"task": "Example", "message_source": "local", "id": "id-1234"},
+            "meta": {"foo": "bar"},
+            "payload": {"input": {"anykey": "anyvalue"}}
+        }
+
+        nested_event_local_return = {
+            'input': {'anykey': 'anyvalue'},
+            'config': {'bar': 'baz'},
+            'messageConfig': {
+                'input': '{{$.payload.input}}',
+                'outputs': [{'source': '{{$.input.anykey}}',
+                            'destination': '{{$.payload.out}}'}]}
+        }
+
+        result = self.sled_message.loadNestedEvent(nested_event_local, {})
         assert result == nested_event_local_return
 
     # assignOutputs tests
     def test_result_payload_without_config(self):
         """ Test nestedResponse is returned when no config argument is passed """
-        result = sled_message._message__assignOutputs(nestedResponse, {}, None)
-        assert result['payload'] == nestedResponse
+        result = self.sled_message._message__assignOutputs(self.nested_response, {}, None)
+        assert result['payload'] == self.nested_response
 
     def test_result_payload_without_config_outputs(self):
         """ Test nestedResponse is returned when config has no outputs key/value """
-        result = sled_message._message__assignOutputs(
-            nestedResponse, {}, message_config_without_outputs)
-        assert result['payload'] == nestedResponse
+        message_config_without_outputs = {}
+        result = self.sled_message._message__assignOutputs(
+            self.nested_response, {}, message_config_without_outputs)
+        assert result['payload'] == self.nested_response
 
     def test_result_payload_with_simple_config_outputs(self):
         """ Test payload value is updated when messageConfig contains outputs templates """
-        result = sled_message._message__assignOutputs(
-            nestedResponse, {}, message_config_with_simple_outputs)
+        # messageConfig objects
+        message_config_with_simple_outputs = {
+            'outputs': [{
+                'source': '{{$.input.dataLocation}}',
+                'destination': '{{$.payload}}'
+            }]
+        }
+
+        result = self.sled_message._message__assignOutputs(
+            self.nested_response, {}, message_config_with_simple_outputs)
         assert result['payload'] == 's3://source.jpg'
 
     def test_result_payload_with_nested_config_outputs(self):
@@ -135,8 +118,15 @@ class Test(unittest.TestCase):
         Test nested payload value is updated when messageConfig contains
         outputs templates with child nodes
         """
-        result = sled_message._message__assignOutputs(
-            nestedResponse, {}, message_config_with_nested_outputs)
+        message_config_with_nested_outputs = {
+            'outputs': [{
+                'source': '{{$.input.dataLocation}}',
+                'destination': '{{$.payload.dataLocation}}'
+            }]
+        }
+
+        result = self.sled_message._message__assignOutputs(
+            self.nested_response, {}, message_config_with_nested_outputs)
         assert result['payload'] == {'dataLocation': 's3://source.jpg'}
 
     # createNextEvent tests
@@ -144,14 +134,14 @@ class Test(unittest.TestCase):
         """
         Test 'replace' key is deleted from value returned from createNextEvent
         """
-        result = sled_message.createNextEvent(
-            nestedResponse, event_with_replace, None)
+        result = self.sled_message.createNextEvent(
+            self.nested_response, self.event_with_replace, None)
         assert 'replace' not in result
 
     def test_small_result_returns_event(self):
         """ Test return result is the event result when it's not too big """
-        result = sled_message.createNextEvent(
-            nestedResponse, event_without_replace, None)
+        result = self.sled_message.createNextEvent(
+            self.nested_response, self.event_without_replace, None)
         expected_result = {
             'input': ':baby_whale:',
             'exception': 'None',
@@ -168,14 +158,24 @@ class Test(unittest.TestCase):
         Test remote event is stored in S3 and return value points
         to remote location with 'replace' key/value
         """
-        uuid_mock.return_value = test_uuid
-        create_next_event_result = sled_message.createNextEvent(
-            nestedResponse, event_with_ingest, None)
+
+        event_with_ingest = {
+            'ingest_meta': {
+                'message_bucket': self.bucket_name
+            },
+            'cumulus_meta': {
+                'workflow': 'testing'
+            }
+        }
+
+        uuid_mock.return_value = self.test_uuid
+        create_next_event_result = self.sled_message.createNextEvent(
+            self.nested_response, event_with_ingest, None)
         expected_create_next_event_result = {
             'cumulus_meta': {'workflow': 'testing'},
-            'replace': {'Bucket': bucket_name, 'Key': next_event_object_key_name}
+            'replace': {'Bucket': self.bucket_name, 'Key': self.next_event_object_key_name}
         }
-        remote_event = s3.Object(bucket_name, next_event_object_key_name).get()
+        remote_event = self.s3.Object(self.bucket_name, self.next_event_object_key_name).get()
         remote_event_object = json.loads(
             remote_event['Body'].read().decode('utf-8'))
         expected_remote_event_object = {
