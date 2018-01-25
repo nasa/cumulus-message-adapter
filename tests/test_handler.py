dcc4363b5648676bc5ff0efd1a746bb6c6364831
sethvincent <sethvincent@gmail.com>
2018-01-25 13:52:43 -0500

    fix test_handler.py for nosetests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -19,8 +19,8 @@ def create_event ():
       "payload": { "anykey": "anyvalue" }
     }
 
-def run_test_handler(event, handler_fn):
-    handler_config = {
+def create_handler_config():
+    return {
         "task": {
             "root": path.join(path.dirname(path.realpath(__file__)), "fixtures"),
             "schemas": {
@@ -31,18 +31,16 @@ def run_test_handler(event, handler_fn):
         }
     }
 
-    return handler(event, {}, handler_fn, handler_config) 
-
 class TestSledHandler(unittest.TestCase):
 
     def test_simple_handler(self):
-        def test_fn(event, context):
+        def handler_fn(event, context):
             return event
 
-        response = run_test_handler(create_event(), test_fn)
+        handler_config = create_handler_config()
+        test_event = create_event()
+        response = handler(test_event, {}, handler_fn, handler_config) 
+
         self.assertTrue(response['cumulus_meta']['task'] == 'Example')
         self.assertTrue(response['payload']['input']['anykey'] == 'anyvalue')
         self.assertTrue(response)
-
-if __name__ == "__main__":
-    unittest.main()
