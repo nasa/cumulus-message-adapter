acffd42b695fb022ddd210ff3a8f1f0840a2e592
Aimee Barciauskas <aimee@developmentseed.org>
2018-01-29 18:32:28 -0500

    Update version location

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
-__version__ = imp.load_source('version', 'version.py').__version__
+__version__ = imp.load_source('version', 'message_adapter/version.py').__version__
 
 setup(
     name='cumulus-message-adpater',  # Required
