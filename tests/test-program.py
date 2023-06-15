47f8ce7be718da19127e41d81ef3db1e11e30a6c
ecarton <ethan.j.carton@nasa.gov>
2023-06-15 13:03:39 -0400

    linter updates

--- tests/test-program.py
+++ tests/test-program.py
@@ -13,6 +13,7 @@ from message_adapter import aws
 
 class Test(unittest.TestCase):
     # pylint: disable=attribute-defined-outside-init
+    # pylint: disable=no-self-use
     # pylint: disable=too-many-locals
 
     """ Test class """
@@ -71,7 +72,7 @@ class Test(unittest.TestCase):
                 eoc_count += 1
                 return json.loads(buffer)
         err_string = ''.join([x.decode('utf-8') for x in stream_process.stderr.readlines()])
-        raise Exception(err_string)
+        raise ValueError(err_string)
 
     def write_streaming_input(self, command, proc_input, p_stdin):
         """
