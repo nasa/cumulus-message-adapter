26abf7ce2a4da2ea157067755475173aba641a8e
ecarton <ethan.j.carton@nasa.gov>
2023-10-04 16:50:16 -0400

    attempting to use simplified git release script

---
+++ scripts/git_release.sh
@@ -0,0 +1,30 @@
+
+set -ex
+export VERSION_NUMBER=$(jq --raw-output .version package.json)
+export VERSION_TAG="v$VERSION_NUMBER"
+export LATEST_TAG=$(curl -H \
+  "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/nasa/cumulus-message-adapter/tags | jq --raw-output '.[0].name')
+
+if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
+  echo "tag does not exist for version $VERSION_TAG, creating tag"
+
+  # create git tag
+  git tag -a "$VERSION_TAG" -m "$VERSION_TAG" || echo "$VERSION_TAG already exists"
+  git push origin "$VERSION_TAG"
+fi
+
+export RELEASE_URL=$(curl -H \
+  "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/nasa/cumulus-message-adapter/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
+
+if [ -z "$RELEASE_URL" ]; then
+  echo "release does not exist"
+
+  curl -H \
+    "Authorization: token $GITHUB_TOKEN" \
+    -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
+    -H "Content-Type: application/json"\
+    -X POST \
+    https://api.github.com/repos/nasa/cumulus-message-adapter/releases
+fi
