ca7dc7b825013e54f60467b331abb3ada3384d7d
ecarton <ethan.j.carton@nasa.gov>
2023-10-06 14:04:23 -0400

    first pass at gpg

--- scripts/git_release.sh
+++ scripts/git_release.sh
@@ -1,9 +1,13 @@
 
 set -ex
+apt update -y && apt install gpg
 VERSION_TAG=`awk -F\' '{print $2,$4}' ../message_adapter/version.py`
+echo "${GPGKEY}" > secret.gpg
 
+gpg --import segret.gpg
 git config --global user.email "cumulus.bot@gmail.com"
 git config --global user.name "cumulus-bot"
+gpg import 
 export LATEST_TAG=$(curl -H \
   "Authorization: token $GITHUB_TOKEN" \
   https://api.github.com/repos/nasa/cumulus-message-adapter/tags | jq --raw-output '.[0].name')
@@ -12,8 +16,8 @@ if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
   echo "tag does not exist for version $VERSION_TAG, creating tag"
 
   # create git tag
-  git tag -a "$VERSION_TAG" -m "$VERSION_TAG" && git tag -v "$VERSION_TAG"
-  git push https://cumulus-bot:${GITHUB_TOKEN}@github.com/nasa/cumulus-message-adapter "$VERSION_TAG"
+  git tag -a "$VERSION_TAG" -m "$VERSION_TAG" -s && git tag -v "$VERSION_TAG"
+  git push origin "$VERSION_TAG"
 fi
 
 export RELEASE_URL=$(curl -H \
