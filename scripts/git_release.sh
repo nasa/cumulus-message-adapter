18bdc34b9001b536e69725bbcea862a594c2a349
ecarton <ethan.j.carton@nasa.gov>
2023-10-06 14:08:13 -0400

    clarifying location space

--- scripts/git_release.sh
+++ scripts/git_release.sh
@@ -3,7 +3,7 @@ set -ex
 sudo apt update -y && sudo apt install gpg
 ls
 ls ..
-VERSION_TAG=`awk -F\' '{print $2,$4}' ../message_adapter/version.py`
+VERSION_TAG=`awk -F\' '{print $2,$4}' ./message_adapter/version.py`
 echo "${GPGKEY}" > secret.gpg
 
 gpg --import secret.gpg
